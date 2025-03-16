c4de0af870ce240c327b25940109dbae44477582
Mohammad568591 <mohamadmashaykhi568591@gmail.com>
2025-03-16 11:55:17 +0330

    Start

---
+++ src/main/java/Project/Final_Project/FinalProjectApplication.java
@@ -0,0 +1,14 @@
+package Project.Final_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(FinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Project/Final_Project/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Project.Final_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
